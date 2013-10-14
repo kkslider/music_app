511c22ad36cc61ea2f3ee07d22d7ffafa5b9d3f6
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-14 12:06:37 -0700

    Fill out BandsController :show, :create, :new

--- music_app/app/controllers/bands_controller.rb
+++ music_app/app/controllers/bands_controller.rb
@@ -3,12 +3,23 @@ class BandsController < ApplicationController
   end
   
   def show
+    @band = Band.find(params[:id])
+    render :show
   end
   
   def new
+    render :new
   end
   
   def create
+    @band = Band.new(params["band"])
+    
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :new
+    end
   end
   
   def edit
