d2114d496a6070e445d6300b31f26e552ae679cb
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-14 11:21:41 -0700

    Create tracks table

--- music_app/db/schema.rb
+++ music_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131014181919) do
+ActiveRecord::Schema.define(:version => 20131014182033) do
 
   create_table "albums", :force => true do |t|
     t.string   "name",       :null => false
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(:version => 20131014181919) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "tracks", :force => true do |t|
+    t.string   "name",       :null => false
+    t.integer  "album_id",   :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "tracks", ["album_id"], :name => "index_tracks_on_album_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",           :null => false
