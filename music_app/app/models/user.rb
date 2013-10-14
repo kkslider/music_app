565737268b5577b486f89b286dde17b329b75161
Benjamin Lee <benjamsandwich@gmail.com>
2013-10-14 10:21:56 -0700

    Create User model

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,3 +1,35 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  attr_accessible :email, :password_digest, :session_token
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  before_validation(on: :create) do
+    self.reset_session_token! unless self.session_token
+  end
   
+  def self.generate_session_token
+    token = SecureRandom.urlsafe_base64(16)
+  end
+  
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+  end
+  
+  def password=(secret)
+    self.password_digest = BCrypt::Password.create(secret)
+    self.save!
+  end
+  
+  def is_password?(secret)
+    BCrypt::Password.create(self.password_digest).is_password?(secret)
+  end
+  
+  def self.find_by_credentials(email, secret)
+    user = User.find_by_email(email)
+    if user
+      user.is_password?(secret)
+    end
+  end
 end
